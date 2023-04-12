53d1e6bbad5fb40d7f871f4a4214cf0720c3ce24
Nickolas Carlos <nickolascarlos@gmail.com>
2023-04-12 14:16:21 -0300

    Adiciona mais skills a 'Outras ferramentas'

--- src/curriculum/OtherTools.jsx
+++ src/curriculum/OtherTools.jsx
@@ -6,7 +6,7 @@ const iconStyle = {fontSize: '18pt'}
 
 export default function OtherTools(props) {
     return (
-        <Section title="Outras Tecnologias">
+        <Section title="Outras Ferramentas">
             <Heap perRow={2.1}>
                 <SkillBar
                     name="SQL"
@@ -33,6 +33,13 @@ export default function OtherTools(props) {
                     level="40%"
                     icon={<i class="devicon-git-plain colored" style={iconStyle}></i>}
                 />
+
+                <SkillBar
+                    name="React"
+                    level="80%"
+                    icon={<i class="devicon-react-plain colored" style={iconStyle}></i>}
+                />
+
                 <SkillBar
                     name="HTML"
                     level="100%"
@@ -52,16 +59,59 @@ export default function OtherTools(props) {
                 />
 
                 <SkillBar
+                    name="Electron"
+                    level="60%"
+                    icon={<i class="devicon-electron-original colored" style={iconStyle}></i>}
+                />
+
+                <SkillBar
                     name="Arduino"
                     level="80%"
                     icon={<i class="devicon-arduino-plain colored" style={iconStyle}></i>}
                 />
 
-                <SkillBar
-                    name="React.js"
-                    level="80%"
-                    icon={<i class="devicon-react-plain colored" style={iconStyle}></i>}
+                <SkillBar 
+                    name="Latex"
+                    level="40%"
+                    icon={<i class="devicon-latex-original colored" style={iconStyle}></i>}
                 />
+
+                <SkillBar
+                    name="Linux"
+                    level="60%"
+                    icon={<i class="devicon-linux-plain colored" style={iconStyle}></i>}
+                />
+            
+                <SkillBar
+                    name="Markdown"
+                    level="80%"
+                    icon={<i class="devicon-markdown-original colored" style={iconStyle}></i>}
+                />
+
+                <SkillBar
+                    name="NPM"
+                    level="80%"
+                    icon={<i class="devicon-npm-original-wordmark colored" style={iconStyle}></i>}
+                />
+
+                <SkillBar
+                    name="Socket.io"
+                    level="80%"
+                    icon={<i class="devicon-socketio-original colored" style={iconStyle}></i>}
+                />
+
+                <SkillBar
+                    name="VS Code"
+                    level="100%"
+                    icon={<i class="devicon-vscode-plain colored" style={iconStyle}></i>}
+                />
+
+                <SkillBar
+                    name="Visual Studio"
+                    level="20%"
+                    icon={<i class="devicon-visualstudio-plain colored" style={iconStyle}></i>}
+                />
+          
             </Heap>
         </Section>
     )
